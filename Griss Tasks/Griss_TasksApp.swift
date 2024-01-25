77fad3595f1fa48e7092849b5926dda404ba3faa
Christian Sanches <christian@endless-point.org>
2024-01-25 16:25:24 -0300

    Initial Commit

---
+++ Griss Tasks/Griss_TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Griss_TasksApp.swift
+//  Griss Tasks
+//
+//  Created by Christian Sanches on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Griss_TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
